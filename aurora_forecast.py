b46557e8abe2bdcb9775e10ee8c0c92a21010571
cmoestl <christian.moestl@oeaw.ac.at>
2019-04-26 16:55:35 +0200

    real time predstorm input added

--- aurora_forecast.py
+++ aurora_forecast.py
@@ -16,7 +16,6 @@ matplotlib.use('Qt5Agg')
 
 import urllib
 from urllib.request import urlopen
-
 from io import StringIO
 from sunpy.time import parse_time
 import numpy as np
@@ -32,9 +31,11 @@ import datetime
 import skimage.transform
 import scipy
 import aacgmv2
+import pdb
+import time
 
-from ovationpyme import ovation_prime
-from ovationpyme import ovation_utilities
+import ovation_prime_predstorm as opp
+import ovation_utilities_predstorm as oup
 
 
 ##################################### FUNCTIONS
@@ -42,174 +43,63 @@ from ovationpyme import ovation_utilities
 
 
 
-def calc_avg_solarwind_predstorm(dt):
-    """
-    Calculates a weighted average of several
-    solar wind variables n_hours (4 by default) backward
-    in time from the closest hourly OMNIWeb
-    datum to datetime dt
-    """
-
-    #time    matplotlib_time B[nT] Bx   By     Bz   N[ccm-3] V[km/s] Dst[nT]   Kp   aurora [GW]
-    file='predstorm_sample/predstorm_real.txt'
-    l1wind = np.loadtxt(file)
-    # Read forecast date and time
-    l1wind_time=l1wind[:,6] 
-
-    t0_mat=matplotlib.dates.date2num(t0)  
-
-    #find index of closest time to t0
-    closest_time_ind=np.argmin(abs(l1wind_time-t0_mat))
-
-    Bx, By, Bz = l1wind[:,8],l1wind[:,9], l1wind[:,10]
-    V,Ni = l1wind[:,12],l1wind[:,11]
-    #Ec = calc_coupling(Bx, By, Bz, V)
-
-
-    print(mdates.num2date(l1wind_time[closest_time_ind])      )
-
-    n_hours = 4       # hours previous to integrate over
-    prev_hour_weight = 0.65    # reduce weighting by factor of wh each hour back
-    
-    weights = [prev_hour_weight*n_hours_back for n_hours_back in np.arange(n_hours+1)] 
-
-
-    times_for_weight_ind = np.arange(closest_time_ind-n_hours, closest_time_ind+1,1)
-    avgsw = dict()
-    avgsw['Bx'] = np.nansum(Bx[times_for_weight_ind]*weights)/len(times_for_weight_ind)
-    avgsw['By'] = np.nansum(By[times_for_weight_ind]*weights)/len(times_for_weight_ind)
-    avgsw['Bz'] = np.nansum(Bz[times_for_weight_ind]*weights)/len(times_for_weight_ind)
-    avgsw['V'] = np.nansum(V[times_for_weight_ind]*weights)/len(times_for_weight_ind)
-    #avgsw['Ec'] = np.nansum(Ec[om_in_avg]*weights)/len(om_in_avg)
-
-
-
-    return avgsw
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def aurora_now():
-    """
-
-    Returns
-    -------
-    img : numpy array
-        The pixels of the image in a numpy array.
-    img_proj : cartopy CRS
-        The rectangular coordinate system of the image.
-    img_extent : tuple of floats
-        The extent of the image ``(x0, y0, x1, y1)`` referenced in
-        the ``img_proj`` coordinate system.
-    origin : str
-        The origin of the image to be passed through to matplotlib's imshow.
-    dt : datetime
-        Time of forecast validity.
-
-    """
-
-    #GitHub gist to download the example data from
-    #url = ('https://gist.githubusercontent.com/belteshassar/'
-    #       'c7ea9e02a3e3934a9ddc/raw/aurora-nowcast-map.txt')
-    # To plot the current forecast instead, uncomment the following line
-    url = 'http://services.swpc.noaa.gov/text/aurora-nowcast-map.txt'
-
-    response_text = StringIO(urlopen(url).read().decode('utf-8'))
-    img = np.loadtxt(response_text)
-    # Read forecast date and time
-    response_text.seek(0)
-    for line in response_text:
-        if line.startswith('Product Valid At:', 2):
-            dt = datetime.strptime(line[-17:-1], '%Y-%m-%d %H:%M')
-
-    img_proj = ccrs.PlateCarree()
-    img_extent = (-180, 180, -90, 90)
-    return img, img_proj, img_extent, 'lower', dt
-
-
-
-
-def aurora_cmap():
-    """Return a colormap with aurora like colors"""
-    stops = {'red': [(0.00, 0.1725, 0.1725),
-                     (0.50, 0.1725, 0.1725),
-                     (1.00, 0.95, 0.95)],
-
-             'green': [(0.00, 0.9294, 0.9294),
-                       (0.50, 0.9294, 0.9294),
-                       (1.00, 0., 0.)],
-
-             'blue': [(0.00, 0.3843, 0.3843),
-                      (0.50, 0.3843, 0.3843),
-                      (1.00, 0., 0.)],
-
-             'alpha': [(0.00, 0.0, 0.0),
-                       (0.50, 1.0, 1.0),
-                       (1.00, 1.0, 1.0)]}
-
-    return LinearSegmentedColormap('aurora', stops)
-
-def aurora_cmap2 ():
-    """Return a colormap with aurora like colors"""
-    stops = {'red': [(0.00, 0.1725, 0.1725),
-                     (0.50, 0.1725, 0.1725),
-                     (1.00, 0.8353, 0.8353)],
-
-             'green': [(0.00, 0.9294, 0.9294),
-                       (0.50, 0.9294, 0.9294),
-                       (1.00, 0.8235, 0.8235)],
-
-             'blue': [(0.00, 0.3843, 0.3843),
-                      (0.50, 0.3843, 0.3843),
-                      (1.00, 0.6549, 0.6549)],
-
-             'alpha': [(0.00, 0.0, 0.0),
-                       (0.50, 1.0, 1.0),
-                       (1.00, 1.0, 1.0)]}
-
-    return LinearSegmentedColormap('aurora', stops)
 
 
 
 ########################################### Main ####################################
 
 
+#initialize
 
-t0 = parse_time("2019-Apr-16 08:00")
+#test
+#t0 = parse_time("2019-Apr-16 06:00")
+#inputfile='predstorm_sample/predstorm_real.txt'
 
-sw=calc_avg_solarwind_predstorm(t0)
+#set time
+
+#t0 = parse_time("2019-May-01 23:00")
+t0 = parse_time("2019-Apr-26 16:00")
+
+#real time
+inputfile='/Users/chris/python/predstorm/predstorm_real.txt'
+
+#make solarwind with averaging over last 4 hours
+sw=oup.calc_avg_solarwind_predstorm(t0,inputfile,4)
+
+print()
+print('Running OVATION aurora model with PREDSTORM input')
+print()
+print()
+print('input file',inputfile)
+
+
+print('time',t0)
+print()
+
+print('solar wind input from weighting procedure:')
+print(sw)
+#for Ec, a cycle average is 4421
+print('Newell coupling compared to solar cycle average: ',np.round(sw['Ec']/4421,2))
+
+#sw=calc_avg_solarwind(t0,'predstorm_sample/predstorm_real.txt')
+#print(sw)
+
+
+#t0 = parse_time("2010-Apr-5 08:00")
 
 
 
-
-############################################# run ovationpyme
-
-
-
-
-sys.exit()
 
 #electron energy flux - mono diff wave needed
+
+print()
+print('Now run own OVATION developed from OvationPyme')
 
 '''
 atype - str, ['diff','mono','wave','ions']
@@ -224,58 +114,44 @@ jtype - int or str
             6:"ion average energy"
 '''
 jtype = 'electron energy flux'
-de = ovation_prime.FluxEstimator('diff', jtype)
-me = ovation_prime.FluxEstimator('mono', jtype)
-we = ovation_prime.FluxEstimator('wave', jtype)
+de = opp.FluxEstimator('diff', jtype)
+me = opp.FluxEstimator('mono', jtype)
+we = opp.FluxEstimator('wave', jtype)
+
+
+
+############################################# run ovationpyme
+
 #ts = [t0 + datetime.timedelta(hours=i) for i in range(1, 24,1)]
 #print(ts)
 #for k in np.arange(1,24):
 #    print(k)
-mlatN, mlonN, fluxNd=de.get_flux_for_time(t0, hemi='N')
-mlatN, mlonN, fluxNm=me.get_flux_for_time(t0, hemi='N')
-mlatN, mlonN, fluxNf=we.get_flux_for_time(t0, hemi='N')
 
+
+
+start = time.time()
+
+
+
+print('Fluxes for northern hemisphere')
+mlatN, mlonN, fluxNd=de.get_flux_for_time(t0,inputfile, hemi='N')
+mlatN, mlonN, fluxNm=me.get_flux_for_time(t0,inputfile, hemi='N')
+mlatN, mlonN, fluxNf=we.get_flux_for_time(t0,inputfile, hemi='N')
 
 #sum all fluxes
 fluxN=fluxNd+fluxNm+fluxNf
 
 
-#interpolate onto full Earth grid later
-#   1024 values covering 0 to 360 degrees in the horizontal (longitude) direction  (0.32846715 degrees/value)
-#   512 values covering -90 to 90 degrees in the vertical (latitude) direction  (0.3515625 degrees/value)
-#   Values range from 0 (little or no probability of visible aurora) to 100 (high probability of visible aurora)
-#mlonN360=mlonN*15     
-#https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.griddata.html#scipy.interpolate.griddata
-#make full Earth a 1024 (lon) x 512 (lat) image
-#mlon, mlat = np.meshgrid(np.arange(0,360,360/1024.0 ),np.arange(-90,90,180/512.0))
-#for i in np.arange(0,79,1):
-#  points=points.append(mlatN,[024/96 ]
-#mx, my = np.mgrid[-180:360/1024:180j, -90:180/512:90j]  
-#aimg = scipy.interpolate.griddata(points, fluxN, (mx, my), method='cubic')#linear, nearest
-#resize image
-#aimg = skimage.transform.resize(fluxN, (512,1024), anti_aliasing=False)
-#Aurora Specification Tabular Values
-# Product: Ovation Aurora Short Term Forecast    <path to tabular data>
-# Product Valid At: 2019-04-15 10:15
-# Product Generated At: 2019-04-15 09:45
-#
-# Prepared by the U.S. Dept. of Commerce, NOAA, Space Weather Prediction Center.
-# Please send comments and suggestions to SWPC.Webmaster@noaa.gov
-#
-# Missing Data:  (n/a)
-# Cadence:   5 minutes
-#
-# Tabular Data is on the following grid
-#
-#   1024 values covering 0 to 360 degrees in the horizontal (longitude) direction  (0.32846715 degrees/value)
-#   512 values covering -90 to 90 degrees in the vertical (latitude) direction  (0.3515625 degrees/value)
-#   Values range from 0 (little or no probability of visible aurora) to 100 (high probability of visible aurora)
+
+print('for aurora maps we use the ',jtype, ' with diff, mono, wave')
 
 
 
+print()
+print('image rescaling ...')
 
 #rescale image
-aimg = skimage.transform.resize(fluxN, (512,1024), anti_aliasing=False)
+aimg = skimage.transform.resize(fluxN, (512,1024), anti_aliasing=False,mode='constant')
 #convert to probabilities
 pimg=10+8*aimg
 #trim small values
@@ -303,10 +179,11 @@ pimg = scipy.ndimage.gaussian_filter(pimg,sigma=(9,9))
 ######################## Coordinate Conversion
 
 print()
-print('Coordinate conversion AACGM -> geographic ')
+print('Coordinate conversion MLT to AACGM mlon/lat to geographic **TODO ')
 
 
-#****************** MLT to mlongtude extra converten??
+#****************** MLT to mlongitude extra converten??
+#mlongitude dreht sich mit mit der Erde wo ist 0deg?
 
 
 #fix grid bug and convert MLT to Mlongitude
@@ -316,8 +193,13 @@ for i in np.arange(1,mlonN.shape[0]):
    mlonN[i]=mlonN[0]
 #now grid is in correct Mlat mlatN / Mlong mlonN
 
+
+startcoo=time.time()
 #Convert to geographic coordinates
 (glatN, glonN, galtN)= aacgmv2.convert_latlon_arr(mlatN,mlonN, 100,t0, code="A2G")
+
+endcoo = time.time()
+print('Coordinate conversion takes in seconds: ', np.round(endcoo-startcoo,3))
 
 #west, east, south, north
 mapextent=(np.min(glonN), np.max(glonN),np.min(glatN), np.max(glatN))
@@ -326,14 +208,89 @@ mapextent=(np.min(glonN), np.max(glonN),np.min(glatN), np.max(glatN))
 #then you would not need to plot a specific part of the image first on mapextent
 
 
-print('.... done.')
+print('done.')
 
+end = time.time()
+
+print('Time needed:  ',np.round(end - start,2),' sec')
+
+print('Calculation for 120 frames would take:', np.round((end - start)*120/60,2),' minutes')
 
 print()
 print()
 
 ########################################## Make aurora plot
 
+plt.close()
+fig = plt.figure(figsize=[10, 10]) 
+
+fig.set_facecolor('black') 
+
+# We choose to plot in an Orthographic projection as it looks natural
+# and the distortion is relatively small around the poles where
+# the aurora is most likely.
+
+# ax1 Europe
+ax1 = plt.subplot(1, 1, 1, projection=ccrs.Orthographic(-100, 90))
+
+
+#nightmap = 'https://map1c.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi'
+#layer = 'VIIRS_CityLights_2012'
+#try: urllib.request.urlretrieve(nightmap,'wmts.cgi')
+#except urllib.error.URLError as e:
+#    print('Failed downloading ', nightmap,' ',e.reason)
+
+
+land_50m = carfeat.NaturalEarthFeature('physical', 'land', '50m',
+                                        edgecolor='k',
+                                        facecolor=carfeat.COLORS['land'])
+                                        
+
+ocean_50m = carfeat.NaturalEarthFeature('physical', 'ocean', '50m',
+                                        edgecolor='k',
+                                        facecolor='steelblue')#carfeat.COLORS['water'])                                        
+                                        
+provinces_50m = carfeat.NaturalEarthFeature('cultural',
+                                             'admin_1_states_provinces_lines',
+                                             '50m',
+                                             facecolor='none',edgecolor='black')
+
+
+crs=ccrs.PlateCarree()
+
+for ax in [ax1]:
+
+    ax.gridlines(linestyle='--',alpha=0.5)
+    #ax.coastlines(alpha=0.5,zorder=3)
+    ax.add_feature(land_50m)
+    #ax.add_feature(carfeat.LAND,zorder=2,alpha=1)
+    ax.add_feature(carfeat.LAKES)#,zorder=2,alpha=1)
+    #ax.add_feature(carfeat.OCEAN)#,zorder=2,alpha=1)
+    ax.add_feature(ocean_50m,linewidth=0.5)
+
+    ax.add_feature(carfeat.BORDERS, alpha=0.5)#,zorder=2,alpha=0.5)
+    #ax.add_feature(carfeat.COASTLINE)#,zorder=2,alpha=0.5)
+    ax.add_feature(carfeat.RIVERS)#,zorder=2,alpha=0.8)
+    ax.add_feature(provinces_50m,alpha=0.5)#,zorder=2,alpha=0.8)
+    #ax.stock_img()
+    
+    #ax.add_wmts(nightmap, layer)
+    ax.add_feature(Nightshade(t0))
+    ax.imshow(pimg, vmin=0, vmax=100, transform=crs,
+    extent=mapextent, origin='lower', zorder=3, alpha=0.9,
+    cmap=oup.aurora_cmap2())
+
+fig.text(0.01,0.92,'PREDSTORM aurora forecast   '+t0.strftime('%Y-%m-%d %H:%M UT' ), color='white',fontsize=15)
+fig.text(0.99,0.02,'C. Möstl / IWF-helio, Austria', color='white',fontsize=8,ha='right')
+
+#exactly full hd resolution with dpi=120 and size 16 9
+fig.savefig('forecast/predstorm_aurora_real_Nhemi_'+t0.strftime("%Y_%m_%d_%H%M")  +'.jpg',dpi=120,facecolor=fig.get_facecolor())
+plt.show()
+
+plt.tight_layout()  
+
+
+sys.exit()
 
 plt.close()
 #16/9 ration for full hd output
@@ -415,7 +372,7 @@ for ax in [ax1, ax2]:
     ax.add_feature(Nightshade(t0))
     ax.imshow(pimg, vmin=0, vmax=100, transform=crs,
     extent=mapextent, origin='lower', zorder=3, alpha=0.9,
-    cmap=aurora_cmap2())
+    cmap=oup.aurora_cmap2())
     if ax == ax1: ax.set_extent([europe_west, europe_east, europe_south, europe_north])
     if ax == ax2: ax.set_extent([canada_west, canada_east, canada_south, canada_north])
 
@@ -429,3 +386,39 @@ fig.text(0.99,0.02,'C. Möstl / IWF-helio, Austria', color='white',fontsize=8,ha
 #exactly full hd resolution with dpi=120 and size 16 9
 fig.savefig('forecast/predstorm_aurora_real_'+t0.strftime("%Y_%m_%d_%H%M")  +'.jpg',dpi=120,facecolor=fig.get_facecolor())
 plt.show()
+
+
+
+
+
+#interpolate onto full Earth grid later
+#   1024 values covering 0 to 360 degrees in the horizontal (longitude) direction  (0.32846715 degrees/value)
+#   512 values covering -90 to 90 degrees in the vertical (latitude) direction  (0.3515625 degrees/value)
+#   Values range from 0 (little or no probability of visible aurora) to 100 (high probability of visible aurora)
+#mlonN360=mlonN*15     
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.griddata.html#scipy.interpolate.griddata
+#make full Earth a 1024 (lon) x 512 (lat) image
+#mlon, mlat = np.meshgrid(np.arange(0,360,360/1024.0 ),np.arange(-90,90,180/512.0))
+#for i in np.arange(0,79,1):
+#  points=points.append(mlatN,[024/96 ]
+#mx, my = np.mgrid[-180:360/1024:180j, -90:180/512:90j]  
+#aimg = scipy.interpolate.griddata(points, fluxN, (mx, my), method='cubic')#linear, nearest
+#resize image
+#aimg = skimage.transform.resize(fluxN, (512,1024), anti_aliasing=False)
+#Aurora Specification Tabular Values
+# Product: Ovation Aurora Short Term Forecast    <path to tabular data>
+# Product Valid At: 2019-04-15 10:15
+# Product Generated At: 2019-04-15 09:45
+#
+# Prepared by the U.S. Dept. of Commerce, NOAA, Space Weather Prediction Center.
+# Please send comments and suggestions to SWPC.Webmaster@noaa.gov
+#
+# Missing Data:  (n/a)
+# Cadence:   5 minutes
+#
+# Tabular Data is on the following grid
+#
+#   1024 values covering 0 to 360 degrees in the horizontal (longitude) direction  (0.32846715 degrees/value)
+#   512 values covering -90 to 90 degrees in the vertical (latitude) direction  (0.3515625 degrees/value)
+#   Values range from 0 (little or no probability of visible aurora) to 100 (high probability of visible aurora)
+
